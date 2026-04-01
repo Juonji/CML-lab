567a7ed1359ce5348916eb62b04f5331cdbdd70c
Juonji <juonji02@gmail.com>
2026-04-01 18:50:36 +0900

    v3

--- train.py
+++ train.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import pandas as pd
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.metrics import ConfusionMatrixDisplay, accuracy_score, confusion_matrix
 from xgboost import XGBClassifier
 
@@ -18,6 +18,19 @@ df['is_child'] = (df['Age'] < 16).astype(int)
 df = df[['Survived', 'Pclass', 'Sex', 'Age', 'Fare', 'family_size', 'is_child']]
 
 
+df['Embarked'] = df['Embarked'].fillna('S')
+
+
+df = pd.get_dummies(df, columns=['Embarked'])
+
+
+df['age_bin'] = pd.cut(
+    df['Age'],
+    bins=[0, 12, 18, 35, 60, 100],
+    labels=False
+)
+
+
 X = df.drop('Survived', axis=1)
 y = df['Survived']
 X_train, X_test, y_train, y_test = train_test_split(
@@ -25,13 +38,34 @@ X_train, X_test, y_train, y_test = train_test_split(
 )
 
 
-model = XGBClassifier()
-model.fit(X_train, y_train)
+params = {
+    'max_depth':[3,4,5],
+    'learning_rate':[0.01,0.05,0.1],
+    'n_estimators':[100,200],
+    'subsample':[0.8,1.0]
+}
 
 
-pred = model.predict(X_test)
+model = XGBClassifier(n_estimators=50, max_depth=2)
+
+
+grid = GridSearchCV(
+    model,
+    params,
+    cv=5,
+    scoring="accuracy"
+)
+
+
+grid.fit(X_train, y_train)
+best_model = grid.best_estimator_
+pred = best_model.predict(X_test)
 acc = accuracy_score(y_test, pred)
+
+
+print("Best params:", grid.best_params_)
 print("Accuracy:", acc)
+
 
 
 cm = confusion_matrix(y_test, pred)
